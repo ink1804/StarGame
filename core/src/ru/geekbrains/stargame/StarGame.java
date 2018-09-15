8237b22be85981d0ec7e9e72ca55ac372454978a
Alexey Kutepov <alkutepov@gmail.com>
2018-09-15 11:10:18 +0300

    Урок 1

---
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		region = new TextureRegion(img, 100, 100, 150, 150);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0.4f, 0.6f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 100, 100);
+		batch.setColor(1, 0.33f, 0.4f, 0.7f);
+		batch.draw(region, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
